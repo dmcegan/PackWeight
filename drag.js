715b75a934a543f7ba99e771aad833953ce686fb
dmcegan <78846869+dmcegan@users.noreply.github.com>
2024-06-06 18:26:53 +1000

    Add files via upload

---
+++ drag.js
@@ -0,0 +1,40 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+  
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+  
+function InventoryDrop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var draggedElement = document.getElementById(data);
+  draggedElement.classList.remove("chart");
+  ev.target.appendChild(draggedElement);
+  ev.target.appendChild(document.getElementById(data));
+  for (const child of draggedElement.children) {
+    child.classList.remove("chart");
+  }
+}
+
+function PackingListDrop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var draggedElement = document.getElementById(data);
+  draggedElement.classList.add("chart");
+  ev.target.appendChild(draggedElement);
+  ev.target.appendChild(document.getElementById(data));
+  for (const child of draggedElement.children) {
+    child.classList.add("chart");
+  }
+}
+
+var counter = 1;
+
+function addInput(){ //Adds a draggable div class whenever the add input button in the inventory is pressed
+  var newdiv = document.createElement('div');
+  newdiv.innerHTML = "<div class='draggable' draggable='true' ondragstart='drag(event)' type='text' id="+counter+"> <input type='text' class='category-input' value='Category 1' onchange='updateChart();'><input type='number' min='0' class='data-input' data-category='Category 1' value='10' onchange='updateChart(); updateTotalWeight();'> <input type='button' value='-' onClick='this.parentNode.remove(); updateChart(); updateTotalWeight();'></div>";
+  document.getElementById('inventory').appendChild(newdiv);
+  counter++;
+}
---
+++ piechart.js
@@ -0,0 +1,51 @@
+var data, options, chart;
+var index = 0;
+
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+index = 0;
+
+// Draw the chart and set the chart values
+function drawChart() {
+  data = new google.visualization.DataTable();
+  data.addColumn('string', 'Item');
+  data.addColumn('number', 'Weight');
+
+  var startingdata = [['Item1', 10], ['Item2', 20], ['Item3', 30]]
+
+  data.addRows(startingdata);
+
+  options = {'width':300, 'height':300, 'legend':'none', chartArea:{left:20,top:20,width:'90%',height:'90%'}};
+
+  // Display the chart inside the <div> element with id="piechart"
+  chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
+
+function updateChart() {
+  var newData = [];
+  var dataInputs = document.querySelectorAll('.data-input.chart');
+  var categoryInputs = document.querySelectorAll('.category-input.chart');
+  
+  dataInputs.forEach(function(dataInput, index) {
+    var categoryInput = categoryInputs[index];
+    newData.push([categoryInput.value, parseFloat(dataInput.value)]);
+  });
+  
+  data = google.visualization.arrayToDataTable([['Item', 'Weight']].concat(newData));
+  chart.draw(data, options);
+}
+
+function updateTotalWeight() {
+  totalweight = 0;
+  for (var i = 0; i < data.getNumberOfRows(); i++) {
+    totalweight += data.getValue(i, 1);
+  }
+  document.getElementById('totalweight').innerHTML = "Total Weight: " + totalweight + "     ";
+}
+
+//setInterval(function() {
+  // code to be executed repeatedly
+//  updateTotalWeight();
+//}, 200);
