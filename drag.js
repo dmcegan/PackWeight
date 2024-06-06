7082b38ab38e6639a7af8c7c92a90dc3ee52e4d0
dmcegan <daniel.mcegan@gmail.com>
2024-06-06 22:11:21 +1000

    Finalised comments

--- drag.js
+++ drag.js
@@ -1,20 +1,19 @@
 function allowDrop(ev) {
-  ev.preventDefault();
+  ev.preventDefault(); //Prevent the default, which is a div cannot be dropped into another div
 }
   
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData("text", ev.target.id); //Sets the dragged item's id to be identified as it's being dragged
 }
   
 function InventoryDrop(ev) {
   ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  var draggedElement = document.getElementById(data);
-  draggedElement.classList.remove("chart");
-  ev.target.appendChild(draggedElement);
-  ev.target.appendChild(document.getElementById(data));
+  var data = ev.dataTransfer.getData("text"); //Get the dragged item's id
+  var draggedElement = document.getElementById(data); //Set draggedElement to the dragged div's id
+  draggedElement.classList.remove("chart"); //Remove "chart" class from dragged element
+  ev.target.appendChild(draggedElement); //Make the dragged element a child of the Inventory div
   for (const child of draggedElement.children) {
-    child.classList.remove("chart");
+    child.classList.remove("chart"); //Remove the "chart" class from the input boxes inside the dragged div
   }
 }
 
@@ -22,19 +21,18 @@ function PackingListDrop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   var draggedElement = document.getElementById(data);
-  draggedElement.classList.add("chart");
+  draggedElement.classList.add("chart"); //Add the "chart" class to the dragged div
   ev.target.appendChild(draggedElement);
-  ev.target.appendChild(document.getElementById(data));
   for (const child of draggedElement.children) {
-    child.classList.add("chart");
+    child.classList.add("chart"); //Add the "chart" class to the input boxes inside the dragged div
   }
 }
 
-var counter = 1;
+var counter = 1; //Counter to set unique item ids
 
 function addInput(){ //Adds a draggable div class whenever the add input button in the inventory is pressed
-  var newdiv = document.createElement('div');
-  newdiv.innerHTML = "<div class='draggable' draggable='true' ondragstart='drag(event)' type='text' id="+counter+"> <input type='text' class='category-input' placeholder='Item Name' onchange='updateChart();'><input type='number' min='0' class='data-input' data-category='Category 1' value='10' onchange='updateChart(); updateTotalWeight();'> <input type='button' value='-' onClick='this.parentNode.remove(); updateChart(); updateTotalWeight();'></div>";
-  document.getElementById('inventory').appendChild(newdiv);
-  counter++;
+  var newdiv = document.createElement('div'); //Created a new div element
+  newdiv.innerHTML = "<div class='draggable' draggable='true' ondragstart='drag(event)' type='text' id="+counter+"> <input type='text' class='category-input' placeholder='Item Name' onchange='updateChart();'><input type='number' min='0' class='data-input' data-category='Category 1' value='10' onchange='updateChart(); updateTotalWeight();'> <input type='button' value='-' onClick='this.parentNode.remove(); updateChart(); updateTotalWeight();'></div>"; //Set's the new div's code to include a text input, a number input and a 'remove' button
+  document.getElementById('inventory').appendChild(newdiv); //Make the new div a child of the 'inventory' div
+  counter++; //Increment the counter for the next unique id
 }
--- piechart.js
+++ piechart.js
@@ -1,4 +1,4 @@
-var data, options, chart;
+var data, options, chart; //Initialise variables for use later
 var index = 0;
 
 // Load google charts
@@ -6,16 +6,16 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 index = 0;
 
-// Draw the chart and set the chart values
 function drawChart() {
-  data = new google.visualization.DataTable();
+  data = new google.visualization.DataTable(); //Create new data table for the values entered into the pie chart
   data.addColumn('string', 'Item');
   data.addColumn('number', 'Weight');
 
-  var startingdata = [['Item1', 10], ['Item2', 20], ['Item3', 30]]
+  var startingdata = [['Item1', 10], ['Item2', 20], ['Item3', 30]] //Set the values for the initial data
 
-  data.addRows(startingdata);
+  data.addRows(startingdata); //Add Initial data to the data table
 
+  //Set options for the pie cart using google charts syntax
   options = {'width':300, 'height':300, 'legend':'none', chartArea:{left:20,top:20,width:'90%',height:'90%'}};
 
   // Display the chart inside the <div> element with id="piechart"
@@ -23,25 +23,25 @@ function drawChart() {
   chart.draw(data, options);
 }
 
-function updateChart() {
+function updateChart() { 
   var newData = [];
-  var dataInputs = document.querySelectorAll('.data-input.chart');
-  var categoryInputs = document.querySelectorAll('.category-input.chart');
+  var dataInputs = document.querySelectorAll('.data-input.chart'); //Identifies all Item value inputs
+  var categoryInputs = document.querySelectorAll('.category-input.chart'); //Identifies all Item name inputs
   
-  dataInputs.forEach(function(dataInput, index) {
+  dataInputs.forEach(function(dataInput, index) {  //For each input identified, add the data to the newData[] array
     var categoryInput = categoryInputs[index];
     newData.push([categoryInput.value, parseFloat(dataInput.value)]);
   });
   
-  data = google.visualization.arrayToDataTable([['Item', 'Weight']].concat(newData));
-  chart.draw(data, options);
+  data = google.visualization.arrayToDataTable([['Item', 'Weight']].concat(newData)); //Concatenate the newData[] array to the existing data table
+  chart.draw(data, options); //Redraw the pie chart
 }
 
 function updateTotalWeight() {
-  totalweight = 0;
-  for (var i = 0; i < data.getNumberOfRows(); i++) {
+  totalweight = 0; //Reset total weight to 0
+  for (var i = 0; i < data.getNumberOfRows(); i++) { //For every row in the data table, add the value to totalweight
     totalweight += data.getValue(i, 1);
   }
-  roundedtotalweight = totalweight.toFixed(2);
-  document.getElementById('totalweight').innerHTML = "Total Weight: " + roundedtotalweight + "     ";
+  roundedtotalweight = totalweight.toFixed(2); //Round the value to 2 d.p. to eliminate binary floating point errors
+  document.getElementById('totalweight').innerHTML = "Total Weight: " + roundedtotalweight + "     "; //Update the totalweight label
 }
